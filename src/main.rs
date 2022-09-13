843b0daea3631e3157d2b614e35786f4f38ac462
Saing <saing.sab@gmail.com>
2022-09-13 14:50:30 +0700

    loading ENV and CONFIG

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ use std::fs;
 
 
 // For ENV
-// #[macro_use]
-// extern crate dotenv_codegen;
+#[macro_use]
+extern crate dotenv_codegen;
 fn main() {
     println!("init... calling env  ");
+    println!("{}", dotenv!("MEANING_OF_LIFE"));
     println!("init... calling Configuration File  ");
     let path = "./storeconfiguration.json";
     let data = fs::read_to_string(path).expect("Unable to read file");
